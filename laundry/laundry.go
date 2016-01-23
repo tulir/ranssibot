557037b777eeb2ef537bde0bdeaf596289f7bc8b
Tulir293 <tulir293@maunium.net>
2016-01-23 17:55:37 +0200

    Some finishing touches

--- laundry/laundry.go
+++ laundry/laundry.go
@@ -11,8 +11,6 @@ import (
 	"time"
 )
 
-var laundry = make(map[string]int)
-
 var notified = 0
 var day = 0
 
@@ -24,10 +22,7 @@ var notifyMinutes = []int{
 	18*60 + 45,
 }
 
-var notifMinDiff = 5
-var notifMaxDiff = 5
-
-// Loop TODO: make comment
+// Loop is an infinite loop that checks if the time is right to spam about laundry turns.
 func Loop(bot *telebot.Bot) {
 	for {
 		now := time.Now()
@@ -41,18 +36,17 @@ func Loop(bot *telebot.Bot) {
 			continue
 		}
 		minsNow := minutesInDay(now)
-		if minsNow >= notifyMinutes[notified]+notifMaxDiff {
+		if minsNow >= notifyMinutes[notified]+5 {
 			// If the notified status is incorrect (as in the notify point was already over notifMaxDiff minutes ago)
 			// increment the notified status and continue loop.
 			notified++
 			log.Debugf("Skipping notifications for laundry turn #%d", notified)
 			continue
-		} else if minsNow >= notifyMinutes[notified]-notifMinDiff {
+		} else if minsNow >= notifyMinutes[notified]-5 {
 			notified++
-			log.Debugf("Sending notifications for laundry turn #%d", notified)
-			Notify(bot, notified)
+			notify(bot, notified)
 		}
-		time.Sleep(1 * time.Minute)
+		time.Sleep(2 * time.Minute)
 	}
 }
 
@@ -60,8 +54,7 @@ func minutesInDay(time time.Time) int {
 	return time.Hour()*60 + time.Minute()
 }
 
-// Notify TODO: make comment
-func Notify(bot *telebot.Bot, time int) {
+func notify(bot *telebot.Bot, time int) {
 	// Get the timetable page
 	doc, err := util.HTTPGetMinAndParse("http://ranssi.paivola.fi/pyykit.php")
 	// Check if there was an error
@@ -91,21 +84,27 @@ func Notify(bot *telebot.Bot, time int) {
 		laundrynode = laundrynode.NextSibling
 	}
 
-	// TODO uncomment to enable attribute checking.
-	/*for _, attr := range laundrynode.Attr {
+	for _, attr := range laundrynode.Attr {
 		// If the current node is not marked as busy, don't send any notifications.
-		if attr.Key == "class" && attr.Val != "busy" {
-			println(attr.Val)
+		if attr.Key == "class" && attr.Val == "free" {
+			log.Debugf("Laundry turn #%d is marked as free, skipping.", notified)
 			return
 		}
-	}*/
+	}
+
 	curName := laundrynode.LastChild.Data
+	curNameLower := strings.ToLower(curName)
+	if len(curName) == 0 {
+		log.Debugf("Laundry turn #%d is empty, skipping.", notified)
+		return
+	}
+	log.Debugf("Sending notifications for laundry turn #%d", notified)
 	for _, user := range config.GetAllUsers() {
 		str, ok := user.GetSetting("laundry")
 		if ok {
 			names := strings.Split(str, ", ")
 			for _, name := range names {
-				if curName == name {
+				if curNameLower == name {
 					bot.SendMessage(user, lang.Translatef(user, "laundry.soon", curName), util.Markdown)
 				}
 			}
@@ -121,15 +120,20 @@ func HandleCommand(bot *telebot.Bot, message telebot.Message, args []string) {
 	}
 	if len(args) > 1 {
 		if util.CheckArgs(args[0], "listen", "watch", "sub", "subscribe", "spam") {
+			if util.CheckArgs(args[1], "clear", "empty") {
+				bot.SendMessage(message.Chat, lang.Translatef(sender, "laundry.subscriptionscleared"), util.Markdown)
+				return
+			}
 			var buf bytes.Buffer
 			for n, arg := range args[1:] {
 				buf.Write([]byte(arg))
+				bot.SendMessage(message.Chat, lang.Translatef(sender, "laundry.subscribed", arg), util.Markdown)
 				if n+2 < len(args) {
 					buf.Write([]byte(", "))
 				}
 			}
-			sender.SetSetting("laundry", buf.String())
-			bot.SendMessage(message.Chat, lang.Translatef(sender, "laundry.subscribed", args[1]), util.Markdown)
+			listenFor := strings.ToLower(buf.String())
+			sender.SetSetting("laundry", listenFor)
 		}
 	} else {
 		bot.SendMessage(message.Chat, lang.Translatef(sender, "laundry.usage"), util.Markdown)
